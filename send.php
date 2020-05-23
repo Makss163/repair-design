e9ffeb8ff248876730af867fbb4beaddd9e248af
Maksim <maksr.2019@mail.ru>
2020-05-23 15:34:13 +0400

    fix: add UTF-8 in send.php

--- send.php
+++ send.php
@@ -14,7 +14,8 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0; 
+    $mail->CharSet = 'UTF-8';                    // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
